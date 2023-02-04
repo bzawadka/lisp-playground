9b627dfba90624f69947d6499bef195534c1dacd
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-02-04 23:06:57 +0100

    wizard game - paths

--- wizard-game.lisp
+++ wizard-game.lisp
@@ -1,4 +1,4 @@
-(defparameter *nodes* '((living-room (you are in the living room. 
+(defparameter *nodes* '((living-room (you are in the living room.
 					  a wizard is snoring loudly on the couch.))
 			(garden (you are in a beautiful garden.
 				     there is a well in front of you.))
@@ -8,3 +8,10 @@
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 
+(defparameter *edges* '((living-room (garden west door)
+                                     (attic upstairs ladder))
+                       (garden (living-room east door))
+                       (attic (living-room downstairs ladder))))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
