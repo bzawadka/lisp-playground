2a633f020ca331753aac953427048fa49c385ed0
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-02-05 11:22:12 +0100

    wizard game - describing paths

--- wizard-game.lisp
+++ wizard-game.lisp
@@ -13,5 +13,18 @@
                        (garden (living-room east door))
                        (attic (living-room downstairs ladder))))
 
+; quasiquoting - create chunks of data that have small pieces of Lisp code embedded in them
 (defun describe-path (edge)
-  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+  `(there is a ,(caddr edge) going ,(cadr edge) to ,(car edge)))
+
+; generate descriptions for all edges from a given location
+; mapcar takes another function and a list - then applies this function to every member of a list
+;   (mapcar #'sqrt '(1 2 3 4 5))
+; append joins several lists into one
+;   (append '(mary had) '(a) '(little lamb))
+; apply - you pass it a function and a list of objects
+;    it pretends that the items in the list are separate objects, and passes them to the given function as such
+;   (apply #'append '((mary had) (a) (little lamb)))
+; location - current player's location
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
